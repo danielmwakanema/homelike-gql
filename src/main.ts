3cc32bf7a94dbb318ec8e0cd0a793b2d3ff2c4a1
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-06-11 16:51:19 +0200

    refactor(main): register pipes and interceptors
    - registered a global validation pipe
    - registered a global class serializer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HOMELIKE_API_PORT } from './common/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(new Reflector()));
+
+  await app.listen(HOMELIKE_API_PORT);
 }
 bootstrap();
