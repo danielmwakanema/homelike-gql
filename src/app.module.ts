86af638aa20f658a99b706861b6432a5bfecc0bb
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-06-13 09:17:13 +0200

    refactor(app): register users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { OrmModule } from './orm/orm.module';
 import { CountriesModule } from './countries/countries.module';
 import { CitiesModule } from './cities/cities.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { CitiesModule } from './cities/cities.module';
     GraphQLModule.forRoot({ autoSchemaFile: true }),
     CountriesModule,
     CitiesModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
