d83ddb3f00de507a64c3523415f114043a70a056
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-06-14 08:38:05 +0200

    refactor(users): add auth guards

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,7 +1,10 @@
 import { Resolver, Query, Args, Int } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 
+@UseGuards(GqlAuthGuard)
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
