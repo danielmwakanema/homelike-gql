140bbd37ba070e7ff4f3004b8851aef311897a5d
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-06-14 20:17:10 +0200

    refactor(env): increase default jwt ttl

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,2 +1,2 @@
-export const JWT_TTL = process.env['JWT_TTL'] || '1000s';
+export const JWT_TTL = process.env['JWT_TTL'] || '10000000s';
 export const JWT_SECRET_KEY = process['JWT_SECRET_KEY'] || 'thisisasecret';
