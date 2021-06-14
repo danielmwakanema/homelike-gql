0ae26eb23ea2faa2ffd01da1d4cf2c1ed30027c3
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-06-14 08:38:32 +0200

    refactor(cities): add auth guards

--- src/cities/cities.resolver.ts
+++ src/cities/cities.resolver.ts
@@ -3,11 +3,14 @@ import { CitiesService } from './cities.service';
 import { City } from './entities/city.entity';
 import { CreateCityInput } from './dto/create-city.input';
 import { UpdateCityInput } from './dto/update-city.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 
 @Resolver(() => City)
 export class CitiesResolver {
   constructor(private readonly citiesService: CitiesService) {}
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => City)
   createCity(@Args('createCityInput') createCityInput: CreateCityInput) {
     return this.citiesService.create(createCityInput);
@@ -23,11 +26,13 @@ export class CitiesResolver {
     return this.citiesService.findOne(id);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => City)
   updateCity(@Args('updateCityInput') updateCityInput: UpdateCityInput) {
     return this.citiesService.update(updateCityInput.id, updateCityInput);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => City)
   async removeCity(@Args('id', { type: () => Int }) id: number) {
     return this.citiesService.remove(id);
