d05a3bcfe36b051313de86786bdf8d2585c509e9
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-06-15 04:08:17 +0200

    chore(apartments): rename query file

--- src/apartments/apartments.module.ts
+++ src/apartments/apartments.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ApartmentsService } from './apartments.service';
 import { ApartmentsResolver } from './apartments.resolver';
-import { ApartmentLocationQuery } from './apartment-location.service';
+import { ApartmentLocationQuery } from './apartment-location.query';
 
 @Module({
   providers: [ApartmentsResolver, ApartmentsService, ApartmentLocationQuery],
